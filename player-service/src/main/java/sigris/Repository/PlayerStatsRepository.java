e1c9609fd7b27d7ff39668a2123a086f3d5a4849
Kharev Yaroslav <409788@niuitmo.ru>
2025-07-29 17:23:27 +0300

    feat: add annotation

--- player-service/src/main/java/sigris/Repository/PlayerStatsRepository.java
+++ player-service/src/main/java/sigris/Repository/PlayerStatsRepository.java
@@ -1,10 +1,12 @@
 package sigris.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sigris.Model.PlayersModel.PlayerStats;
 
 import java.util.Optional;
 
+@Repository
 public interface PlayerStatsRepository extends JpaRepository<PlayerStats, String> {
     Optional<PlayerStats> findByNickName(String nickName);
 }
--- player-service/src/main/java/sigris/Repository/PlayersRepository.java
+++ player-service/src/main/java/sigris/Repository/PlayersRepository.java
@@ -1,10 +1,12 @@
 package sigris.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sigris.Model.PlayersModel.Player;
 
 import java.util.Optional;
 
+@Repository
 public interface PlayersRepository extends JpaRepository<Player, Long> {
     Optional<Player> findByNickName(String Name);
 }
